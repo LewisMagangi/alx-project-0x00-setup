9c7cfcd479d8b2f93e3366b6343a2d5b8523e705
Liquelaliqour <lewismomanyi34@gmail.com>
2025-09-19 15:57:09 +0300

    6. Advanced Task

--- alx-project-0x00/interfaces/index.ts
+++ alx-project-0x00/interfaces/index.ts
@@ -1,3 +1,7 @@
+export interface ButtonProps {
+	title: string;
+	className?: string;
+}
 export interface PillProps {
 	title: string;
 }
--- alx-project-0x00/pages/landing.tsx
+++ alx-project-0x00/pages/landing.tsx
@@ -1,10 +1,17 @@
 import Card from "@/components/Card";
 
+import Button from "@/components/Button";
+
 const Landing: React.FC =  () => {
   return (
     <div>
       <h1 className=" text-xl font-extralight">Landing Page</h1>
       <Card />
+      <div className="flex flex-col gap-4 mt-8">
+        <Button title="Small Rounded-sm" className="text-xs rounded-sm" />
+        <Button title="Medium Rounded-md" className="text-base rounded-md" />
+        <Button title="Large Rounded-full" className="text-lg rounded-full" />
+      </div>
     </div>
   )
 }
